f6f7bee55583d6476f17093b36deef630262991f
autoenclaveislamabadlimited <autoenclaveislamabadlimited@gmail.com>
2025-05-20 02:18:47 +0500

    added file structure and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind + React!</h1>
+    </div>
+  );
+}
+
+export default App
